d02959c6f184390823394c1b8b4d3b512c998658
Bruno <mguimaraesbruno@gmail.com>
2020-06-22 20:23:17 -0300

    select style

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -173,19 +173,21 @@ const pickerSelectStyles = StyleSheet.create({
 		paddingHorizontal: 10,
 		borderWidth: 1,
 		borderColor: 'gray',
-		borderRadius: 4,
+		borderRadius: 8,
 		color: 'black',
 		paddingRight: 30, // to ensure the text is never behind the icon
+		marginBottom: 8
 	},
 	inputAndroid: {
 		fontSize: 16,
 		paddingHorizontal: 10,
 		paddingVertical: 8,
 		borderWidth: 0.5,
-		borderColor: 'purple',
+		borderColor: 'gray',
 		borderRadius: 8,
 		color: 'black',
 		paddingRight: 30, // to ensure the text is never behind the icon
+		marginBottom: 8
 	},
 });
 
